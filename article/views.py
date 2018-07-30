6b1db8b82bdde66f4bb921d760c61588529439df
dlawjdqls10 <dlawjdqls10@naver.com>
2018-07-30 19:36:20 +0900

    세팅

---
+++ article/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def post_list(request):
+    return render(request, 'article/post_list.html')
+
+
+def poster_detail(request):
+    return render(request, 'article/post_detail.html')
+
+
+
+
